648577beceb04a870c8b424b69502c0c4ee703d1
jin-yungi <0531>
2022-09-21 12:21:54 +0900

    base structure

--- app/src/main/java/com/example/market/MainFragAdapter.java
+++ app/src/main/java/com/example/market/MainFragAdapter.java
@@ -1,4 +1,30 @@
 package com.example.market;
 
-public class MainFragAdapter {
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MainFragAdapter extends FragmentStateAdapter {
+
+    public MainFragAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(HomeFragment.EXTRA_POSITION,(position+1));
+        return HomeFragment.getInstance(bundle);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
 }
+
