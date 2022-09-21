3f4a66a0334a8aa38863b2fc0865fc5d43dbaee1
jin-yungi <0531>
2022-09-21 12:22:04 +0900

    base structure

--- app/src/main/java/com/example/market/MainActivity.java
+++ app/src/main/java/com/example/market/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.market;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -8,8 +9,12 @@ import android.widget.ImageView;
 
 import com.example.market.databinding.ActivityMainBinding;
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Integer[] tabIcons = { R.drawable.ic_home_24, R.drawable.ic_search_24, R.drawable.ic_add_24,R.drawable.ic_talk_24,R.drawable.ic_my_24 };
+    private String[] tabNames = {"홈", "검색", "등록", "번개톡", "My"};
 
     private ActivityMainBinding binding;
     private MainFragAdapter adapter;
@@ -24,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        adapter = new MainFragAdapter(getSupportFragmentManager(), getLifecycle());
+        binding.vpMain.setAdapter(adapter);
 
+        new TabLayoutMediator(binding.tabLayout, binding.vpMain, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(tabNames[position]);
+                tab.setIcon(tabIcons[position]);
+            }
+        }).attach();
     }
 }
