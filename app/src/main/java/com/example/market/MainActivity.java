c773aa541c5b57cbe68c521a95f11e9abe4ecace
jin-yungi <0531>
2022-09-21 12:03:40 +0900

    first commit (standard)

--- app/src/main/java/com/example/market/MainActivity.java
+++ app/src/main/java/com/example/market/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.market;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.market.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MainFragAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initView();
+    }
+
+    private void initView() {
+
     }
 }
